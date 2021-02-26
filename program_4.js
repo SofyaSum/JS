9fcd17dc5ce7ed242de6b058ee73dac75b8dc1a3
SofyaSum <55841528+SofyaSum@users.noreply.github.com>
2021-02-26 15:06:19 +0700

    Update program_4.js
    Вывод от заданного числа до 15

--- program_4.js
+++ program_4.js
@@ -1,17 +1,11 @@
 'use strict';
 
-let a = +prompt ('Введите число от 1 до 15');
+var a = +prompt ('Введите число от 1 до 15');
   
 switch (a) {
   case 1:
-    alert('Вы ввели число 1');
-    break;
   case 2:
-    alert('Вы ввели число 2');
-    break;
   case 3:
-    alert('Вы ввели число 3');
-    break; 
   case 4:
   case 5:
   case 6:
@@ -24,7 +18,10 @@ switch (a) {
   case 13:
   case 14:
   case 15:
-    alert('Вы ввели число ' + a);
+    while (a <= 15) {
+      console.log(a);
+      a++;
+    }
     break;                       
   default:
     alert('Неверный ввод');
